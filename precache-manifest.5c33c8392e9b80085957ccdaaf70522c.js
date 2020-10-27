6f15eeacf58f3a591d7e5011c1ff4b40af51e55d
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-27 22:12:28 +0200

    Updates

---
+++ precache-manifest.5c33c8392e9b80085957ccdaaf70522c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528849f3d8100d72da42b13c92f15b32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59803c004862a9c0930b",
+    "url": "./static/css/main.76a22f48.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59803c004862a9c0930b",
+    "url": "./static/js/main.3af9b306.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
